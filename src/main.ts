4b9416f9d74cdaa0d230a805fb8b9ea1be88ce6d
Max Daniels <daniels.g@northeastern.edu>
2021-11-16 09:57:08 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import * as d3 from "d3";
+import * as $ from "jquery";
+import { LetterEdge, PerimeterEdge, PuzzleEdge, COORDINATES } from "./view";
+import { TextGrid } from "./model";
+import { line } from "d3-shape";
+
+export function main() {
+    let text = "z";
+    let grid = new TextGrid(text, 1);
+    let cnf = new COORDINATES(150, [20, 20]);
+
+    let edges = text_to_puzzle(grid, cnf);
+    let lineGenerator = d3.line().curve(d3.curveBasis);
+
+    edges.forEach(edge => {
+        d3.select("#svg")
+          .append("path")
+          .attr("d", lineGenerator(edge.absolute_coordinates()))
+          .attr("stroke", "black")
+          .attr("stroke-width", "3");
+    });
+}
+
+function text_to_puzzle(text: TextGrid, cnf: COORDINATES) {
+    let edges: PuzzleEdge[] = [];
+    // perimeter edges first
+    for(let i=0; i < text.width; i++) {
+        edges.push(new PerimeterEdge(cnf, 0, i, "up"));
+        edges.push(new PerimeterEdge(cnf, text.height-1, i, "down"));
+        //edges.push(new LetterEdge(cnf, text.height-1, i, "right", " "));
+    }
+    for(let j=0; j < text.height; j++) {
+        edges.push(new PerimeterEdge(cnf, j, 0, "left"));
+        edges.push(new PerimeterEdge(cnf, j, text.width-1, "right"));
+        //edges.push(new LetterEdge(cnf, j, text.width-1, "down", " "));
+    }
+
+    // letter edges 
+    for(let j=0; j< text.width - 1; j++) {
+        for(let i=0; i < text.height - 1; i++) {
+            edges.push(new LetterEdge(cnf, i, j, "right", text.get(i, j)));
+            edges.push(new LetterEdge(cnf, i, j, "down", text.get(i, j)));
+        }
+    }
+    return edges;
+}
